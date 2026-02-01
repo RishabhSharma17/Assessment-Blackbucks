72d7ff3ff560f8fd290bf06add9dbf729aa955da
RishabhSharma17 <rishabhsharma17aug2004@gmail.com>
2026-02-02 00:27:46 +0530

    some improvements

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import express, { Request, Response } from "express";
+import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import userRouter from "./routes/user.routes";
--- server/src/utils/jwt.ts
+++ server/src/utils/jwt.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const generateToken = (userId: number, role: string) => {
-  return jwt.sign({ userId, role }, process.env.JWT_SECRET!, {
+  return jwt.sign({ userId, role }, process.env.JWT_SECRET! ?? "Secret", {
     expiresIn: "1d",
   });
 };
